b06f53e56933a3ccf7022dc4664bb084a00d2b7e
Martin Pettersson <kontakt@martinpettersson.se>
2015-06-23 18:38:26 +0900

    Loading images.

--- Main.py
+++ Main.py
@@ -3,9 +3,11 @@ import sys
 
 # Init
 pygame.init()
-screen = pygame.display.set_mode((640, 360), 0, 32)
+WIDTH, HEIGHT = 640,360
+screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 clock = pygame.time.Clock()
 FPS = 24
+img_narwhal = pygame.image.load("single_narwhal.png")
 
 # Colors
 clr1 = (123, 23, 34)
@@ -28,10 +30,7 @@ while True:
 
     # Draw
     screen.fill((changing_color, 40, 243))
-    pygame.draw.circle(screen, clr2, (350, 200), 80, 30)
-    pygame.draw.line(screen, clr1, (0, 0), (640, 360), 5)
-    pygame.draw.rect(screen, clr3, (40, 40, 300, 45))
-
+    screen.blit(img_narwhal, (200, 200))
     # Flip y-axis
     pygame.display.flip()
 
