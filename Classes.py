e394e2c64be6e28f9c13c6e67ae6eb67e11c4158
Martin Pettersson <kontakt@martinpettersson.se>
2015-09-19 22:18:02 +0200

    Added enemy movement

--- Classes.py
+++ Classes.py
@@ -1,7 +1,12 @@
 import pygame
+import math
+import classes
+from random import randint
+
 
 class BaseClass(pygame.sprite.Sprite):
     all_sprites = pygame.sprite.Group()
+
     def __init__(self, x, y, width, height, image_string):
         pygame.sprite.Sprite.__init__(self)
         BaseClass.all_sprites.add(self)
@@ -14,11 +19,13 @@ class BaseClass(pygame.sprite.Sprite):
 
 class Narwhal(BaseClass):
     Narwhals = pygame.sprite.Group()
+
     def __init__(self, x, y, width, height, image_string, velocity):
         BaseClass.__init__(self, x, y, width, height, image_string)
         Narwhal.Narwhals.add(self)
         self.velx = 0
         self.vely = 0
+
     def motion(self, SCREENWIDTH):
         predicted_location = self.rect.x + self.velx
         if predicted_location < 0:
@@ -30,6 +37,21 @@ class Narwhal(BaseClass):
 
 class HelmetFish(BaseClass):
     List = pygame.sprite.Group()
+
     def __init__(self, x, y, width, height, image_string):
         BaseClass.__init__(self, x, y, width, height, image_string)
         HelmetFish.List.add(self)
+        self.velx = randint(1, 4)
+        self.amplitude, self.period = randint(20, 140), randint(4, 5) / 100.0
+
+    def swim(self, SCREENWIDTH):
+        if self.rect.x + self.width > SCREENWIDTH or self.rect.x < 0:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.velx = -self.velx
+        self.rect.x += self.velx
+        self.rect.y = self.amplitude * math.sin(self.period * self.rect.x) + 140
+
+    @staticmethod
+    def movement(SCREENWIDTH):
+        for fish in HelmetFish.List:
+            fish.swim(SCREENWIDTH)
--- Main.py
+++ Main.py
@@ -9,6 +9,8 @@ clock = pygame.time.Clock()
 background = pygame.image.load("images/underwater_background/background2.png")
 narwhal1 = Narwhal(0, SCREENHEIGHT - 90, 261, 92, "images/single_narwhal.png", 7)
 helmetfish = HelmetFish(40, 100, 78, 36, "images/helmetfish.png")
+helmetfish2 = HelmetFish(60, 400, 78, 36, "images/helmetfish.png")
+helmetfish3 = HelmetFish(90, 800, 78, 36, "images/helmetfish.png")
 FPS = 24
 
 # Colors
@@ -21,9 +23,9 @@ changing_color = 0
 while True:
     # Processes
     process(narwhal1)
-
     # Logic
     narwhal1.motion(SCREENWIDTH)
+    HelmetFish.movement(SCREENWIDTH)
 
     #Draw
     screen.blit(background, (0, 0))
